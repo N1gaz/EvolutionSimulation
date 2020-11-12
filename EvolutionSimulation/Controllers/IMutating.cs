2de06cd1d5ef2449ca302490b26c62eb369b8e4a
N1gaz <58673572+N1gaz@users.noreply.github.com>
2020-11-12 23:14:37 +0300

    fix css

--- EvolutionSimulation/Controllers/IMutating.cs
+++ EvolutionSimulation/Controllers/IMutating.cs
@@ -1,19 +1,17 @@
-﻿interface IMutating
+﻿
+using EvolutionSimulation.Models;
+using System;
+
+interface IMutating
 {
-	public void Move(Tuple<float, float> centerFood, Tuple<float, float> red
-			Tuple<float, float> green, Tuple<float, float> blue, float time);
+	void Move(Tuple<float, float> centerFood, Tuple<float, float> red,
+            Tuple<float, float> green, Tuple<float, float> blue, float time);
 
-	public static void Eat<T>(T object);
-
-	public void Init(Genome g)
-
-	public Tuple<float, float> getCoordinates();
-
-	private void setCoordinates(Tuple<int, int> coord);
-
+	void Eat<T>(T obj) where T : class;
+	void Init(Genome g);
+	Tuple<float, float> getCoordinates();
+	void SetCoordinates(Tuple<int, int> coord);
 	public Color getColor();
-
-	private void setColor(Color c);
-
-	public float GetDamage(IMutable source);
+	void SetColor(Color c);
+	public float GetDamage(IMutating source);
 }
